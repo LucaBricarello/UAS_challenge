c38777dd987c6cad16601552e10ed47f736d0430
ThePirate42 <git@thepirate42.org>
2025-06-19 02:53:09 +0200

    Cambiamento codice buzzer

--- BUZZ_not_nominal_land.lua
+++ BUZZ_not_nominal_land.lua
@@ -1,5 +1,6 @@
 local LANDING_RADIUS = 100 -- metri
-local has_beeped = false
+local TAKEOFF_RADIUS = 100 -- metri
+local on_land_time = nil
 
 local function get_mission_location(index)
 
@@ -23,37 +24,41 @@ if not num_commands or num_commands <= 0 then
   gcs:send_text(3, "Errore: missione vuota")
   error("Errore: missione vuota", 0)
 end
-
+local takeoff_center = get_mission_location(0)
+gcs:send_text(7, string.format("Takeoff WP: lat=%.6f, lon=%.6f", takeoff_center:lat() / 1e7, takeoff_center:lng() / 1e7))
 local landing_center = get_mission_location(num_commands - 1)
-
-gcs:send_text(6, string.format("Landing WP: lat=%.6f, lon=%.6f",
-  landing_center:lat() / 1e7, landing_center:lng() / 1e7))
+gcs:send_text(7, string.format("Landing WP: lat=%.6f, lon=%.6f", landing_center:lat() / 1e7, landing_center:lng() / 1e7))
 
 local function update()
 
-  if arming:is_armed() then
-    -- Reset flag se si arma di nuovo
-    has_beeped = false
-    return update, 1000
-  end
+  if vehicle:get_likely_flying() == false then
 
-  -- Se disarmato (a terra presumibilmente)
-  if not has_beeped then
+    local now = tonumber(millis()) or 0
 
-    local loc = ahrs:get_location()
-    if not loc then
-      return update, 1000
+    if on_land_time == nil then
+      on_land_time = now
+    elseif ((now - on_land_time) > 5000) then
+
+      local loc = ahrs:get_location()
+      if not loc then
+        return update, 1000
+      end
+      local landing_dist = landing_center:get_distance(loc)
+      local takeoff_dist = takeoff_center:get_distance(loc)
+
+      if landing_dist > LANDING_RADIUS and takeoff_dist > TAKEOFF_RADIUS then
+        gcs:send_text(1, string.format(
+          "⚠️ Landing fuori zona (%.1f m)!", landing_dist
+        ))
+        notify:play_tune("MFT240L8 O4aO5dc O4aO5dc O4aO5dc O4aO5dc")
+        return update, 4000
+      end
+
     end
 
-    local dist = landing_center:get_distance(loc)
-    if dist > LANDING_RADIUS then
-      gcs:send_text(1, string.format(
-        "⚠️ Landing fuori zona (%.1f m)!", dist
-      ))
-      -- Se vuoi far suonare qualcosa, qui va il comando futuro
-      has_beeped = true
-    end
-
+  else
+    -- L'aereo non è più a terra → resetto timer
+    on_land_time = nil
   end
 
   return update, 1000
